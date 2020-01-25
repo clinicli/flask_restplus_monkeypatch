d75c67452a9304b3987f056000a779e30fed1929
austinjp <austin.plunkett@gmail.com>
2020-01-25 14:50:08 +0000

    Simple API, working.

--- flask_restplus_monkeypatch/monkeypatch.py
+++ flask_restplus_monkeypatch/monkeypatch.py
@@ -1,13 +1,52 @@
-import inspect
+import importlib
 
-from . import plugins
+from . import plugins as _plugins
 from . import const
+from . import utils
 
-def apply(app=None,debug=False,test=False):
-    d = __package__ + "." + const.plugins_dir + "."
-    exclude = [ d + const.example ]
-    if not debug: exclude.append(d + const.debug)
-    if not test: exclude.append(d + const.test)
+def apply(app,plugins=[],exclude=[],debug=False,test=False,example=False,*args,**kwargs):
 
-    for p in [ _ for _ in plugins.discovered_plugins if _ not in exclude ]:
-        print(p)
+    _debug = _test = _example = False
+
+    if debug:
+        kwargs["debug"] = debug if type(debug) is dict else {}
+        _debug = True
+
+    if test:
+        kwargs["test"] = test if type(test) is dict else {}
+        _test = True
+
+    if example:
+        kwargs["example"] = example if type(example) is dict else {}
+        _example = True
+
+    if type(plugins) is str:
+        plugins = [ plugins ]
+    if type(plugins) is not list:
+        raise TypeError("Expected str or list for plugins, got " + str(type(plugins)))
+
+    if type(exclude) is str:
+        exclude = [ exclude ]
+    if type(exclude) is not list:
+        raise TypeError("Expected str or list for exclude, got " + str(type(exclude)))
+
+    if not _example: exclude.append(const.example)
+    if not _debug: exclude.append(const.debug)
+    if not _test: exclude.append(const.test)
+
+    for p in [
+            str(_)
+            for _ in _plugins.discovered_plugins
+            if _ not in exclude ]:
+        if p not in plugins:
+            plugins.append(p)
+
+    for p in plugins:
+        _kwargs = {}
+        if p in kwargs:
+            if type(kwargs[p]) is not dict:
+                raise TypeError("Expected dict, got " + str(type(kwargs[p])))
+            _kwargs = { **kwargs[p] }
+            app = importlib.import_module(utils.module_full_name(p)).init(app,**_kwargs)
+
+    return app
