c17b2f5d6d8ac8e4516494f86bdc5009ebbf411c
austinjp <austin.plunkett@gmail.com>
2020-01-25 11:33:19 +0000

    Beginnings of package setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flask RESTPlus monkeypatches
+
+The reasons why this package exists are to be found here: <https://github.com/noirbizarre/flask-restplus/issues/517>
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flask_restplus_monkepatch",
+    version="0.0.1",
+    author="Austin Plunkett",
+    author_email="austin.plunkett+flask_restplus_monkepatch@gmail.com",
+    description="Apply some necessary patches to the RESTPlus package for Flask.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/austinjp/flask_restplus_monkeypatch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
+
