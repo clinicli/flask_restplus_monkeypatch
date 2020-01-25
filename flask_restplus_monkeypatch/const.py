0881c8ea24dcae23fb465bcaef21f5a3c8ad8267
austinjp <austin.plunkett@gmail.com>
2020-01-25 14:47:17 +0000

    Report the name of the const that the user is trying to change.

--- flask_restplus_monkeypatch/const.py
+++ flask_restplus_monkeypatch/const.py
@@ -12,14 +12,9 @@ class _const:
         self.test = "test"
         self.debug = "debug"
 
-        # Add the dir name of your plugin here:
-        self.os = "os"
-        self.k8s = "k8s"
-        self.https = "https"
-
     def __setattr__(self,name,value):
         if name in self.__dict__.keys():
-            raise self.ConstError("Cannot rebind const.")
+            raise self.ConstError("Const cannot be re-declared: " + str(name))
         self.__dict__[name]=value
 
 sys.modules[__name__]=_const()
